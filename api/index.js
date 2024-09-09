c929fc9b4e2b114279d47b148f80dc4ed575d2ab
Aryan Abrol <aryanabrol95@gmail.com>
2024-09-09 15:19:12 +0530

    second

--- api/index.js
+++ api/index.js
@@ -86,7 +86,7 @@ function extractFirstContent(text, type, originalContent) {
 }
 
 // Start the server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
